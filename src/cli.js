630c02fa0e2713969a036b949805d5c0a2328608
cucumbervegetable <a.taylashev@gmail.com>
2026-04-06 13:33:55 +0700

    end

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,6 @@ import readlineSync from 'readline-sync';
 export const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
+  console.log(name);
   console.log(`Hello, ${name}!`);
 };
