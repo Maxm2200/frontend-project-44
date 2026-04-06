356b3140748b370835acf2ac23a4c410d79cd6e5
Maxim <maximka200557@gmail.com>
2026-04-06 22:53:00 +0700

    fds

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/indent': ['error', 2],
+      'no-console': 'off',
+    },
+  },
+]
