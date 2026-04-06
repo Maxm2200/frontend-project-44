3d5165e409ead14e5b8db5e6ce18a3563fd1c4e5
Maxim <maximka200557@gmail.com>
2026-04-06 22:15:17 +0700

    fs

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -1,13 +1,13 @@
-import startGames from "../index.js"
+import startGames from "../index.js";
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min)
 };
-const getLengthProgression = getRandomNumber(5, 15);
-const getIncrementProgression = getRandomNumber(2, 8);
-const getIndexHiddenElement = getRandomNumber(0, getLengthProgression - 1);
-const getStartElementProgression =  getRandomNumber(2, 30);
 const description = 'What number is missing in the progression?';
 const game = () => {
+    const getLengthProgression = getRandomNumber(5, 15);
+    const getIncrementProgression = getRandomNumber(2, 8);
+    const getIndexHiddenElement = getRandomNumber(0, getLengthProgression - 1);
+    const getStartElementProgression =  getRandomNumber(2, 30);
     const progression = [];
     for (var i = 0; i < getLengthProgression; i+= 1) {
         progression.push(getStartElementProgression + i * getIncrementProgression);
@@ -18,4 +18,4 @@ const game = () => {
     return [question, correct];
 };
 const GamesProgression = startGames(description, game);
-export default GamesProgression;
+export default GamesProgression();
